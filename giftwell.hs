49af8e1114e72238625d90c5cee498069e448888
risingbirdsong <heregoingthere@outlook.com>
2020-09-25 20:26:41 -0700

    getting the Just indexes of source that come from target

--- giftwell.hs
+++ giftwell.hs
@@ -4,4 +4,7 @@ import Data.Ord  -- buy :: (Num a, Eq a) => a -> [a] -> Maybe (Int, Int)
 -- testing is = sortBy  (comparing Down) is -- [5,4,3,2,1]
   -- buy :: (Num a, Eq a) => a -> [a] -> Any
   -- buy :: ((a -> a -> Ordering) -> t1 -> t2) -> t1 -> t2
-buy c is =  foldr (\x acc -> if ((acc + x) < c) then acc + c else acc ) 0 $ sortBy  (comparing  Down) is
+findNums c is = foldr  (\x acc -> if ((sum acc + x) <= c) then x : acc else acc ) [] $ sortBy  (comparing  Down) is
+getIdxs x is = elemIndex x is
+
+composing source target = foldr (\x acc -> if (x `elem` target) then elemIndex x source : acc else acc ) [] source   
