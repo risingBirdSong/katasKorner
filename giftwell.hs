5237cacb96018b69bc5e44bd160ab6f69a21cc3b
risingbirdsong <heregoingthere@outlook.com>
2020-09-25 20:52:16 -0700

    getting close! a mismatch of justs, but the concept is there

--- giftwell.hs
+++ giftwell.hs
@@ -5,6 +5,11 @@ import Data.Ord  -- buy :: (Num a, Eq a) => a -> [a] -> Maybe (Int, Int)
   -- buy :: (Num a, Eq a) => a -> [a] -> Any
   -- buy :: ((a -> a -> Ordering) -> t1 -> t2) -> t1 -> t2
 findNums c is = foldr  (\x acc -> if ((sum acc + x) <= c) then x : acc else acc ) [] $ sortBy  (comparing  Down) is
-getIdxs x is = elemIndex x is
 
+composing :: (Foldable t, Eq a) => [a] -> t a -> [Maybe Int]
 composing source target = foldr (\x acc -> if (x `elem` target) then elemIndex x source : acc else acc ) [] source   
+tupleMaker s t 
+  | length( composing s t ) == 2 =  Just (head (composing s t) , last  (composing s t))
+  | otherwise = Nothing
+
+buy c is = tupleMaker is (findNums  c is)
